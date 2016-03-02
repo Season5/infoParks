1a9672c2640a0c25c339d5f4c13e3964216fc6ec
joseph muli <jayjaymuli00@gmail.com>
2016-03-02 18:59:07 +0300

    added layouts, activities

---
+++ app/src/main/java/com/infoparks/wapi/infoparks/Eastern.java
@@ -0,0 +1,13 @@
+package com.infoparks.wapi.infoparks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Eastern extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.eastern);
+    }
+}
