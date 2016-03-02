29d4baeacd69292c26d1be0b184d3bb2ba8c286a
joseph muli <jayjaymuli00@gmail.com>
2016-03-02 19:16:51 +0300

    splash code

--- app/src/main/java/com/infoparks/wapi/infoparks/Splash.java
+++ app/src/main/java/com/infoparks/wapi/infoparks/Splash.java
@@ -1,13 +1,35 @@
 package com.infoparks.wapi.infoparks;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
 public class Splash extends AppCompatActivity {
+
+
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            public void run() {
+
+                Intent intent = new Intent(Splash.this, Main.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
+
