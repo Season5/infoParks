44b185f8e099cdd9fb59d7c39f0e6dc39ec7036a
ImayaDismas <imayadismas@gmail.com>
2016-03-04 18:20:11 +0300

    Updated files

--- app/src/main/java/com/infoparks/wapi/infoparks/Central.java
+++ app/src/main/java/com/infoparks/wapi/infoparks/Central.java
@@ -8,6 +8,6 @@ public class Central extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.central);
+        setContentView(R.layout.ccentral);
     }
 }
